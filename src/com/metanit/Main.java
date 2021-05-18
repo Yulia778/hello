12b31b2daf314c6ef2f43c06edeba884bc3add1c
Yulia778 <yulchic_95@mail.ru>
2021-05-19 00:16:03 +0300

    Initial commit

---
+++ src/com/metanit/Main.java
@@ -0,0 +1,44 @@
+package com.metanit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello world!");
+
+        int myInt;
+        myInt = 557;
+        System.out.println(myInt);
+        byte myByte = 99;
+        System.out.println(myByte);
+        short myShort = 31000;
+        System.out.println(myShort);
+        long myLong = 90000214;
+        System.out.println(myLong);
+        float myFloat = 2363.5f;
+        System.out.println(myFloat);
+        double myDouble = 3.4;
+        System.out.println(myDouble);
+        char v = 's';
+        System.out.println(v);
+        boolean d = true;
+        System.out.println(d);
+
+        int z = 1;
+        float u = 0.0f;
+        System.out.println(z / u);
+
+        char ch = 1;
+        while (ch > 0) {
+            System.out.println(ch++);}
+
+            int a = 40;
+            int b = 1;
+            System.out.println(a / b);
+
+            int x = 40;
+            int y = 0;
+            System.out.println(x / y);
+
+    }
+}
